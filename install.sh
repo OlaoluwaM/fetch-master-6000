8ca714aceaf761497f58815bf7beb0a1f5da42e6
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-07-26 17:17:39 +0100

    feat: mocked install script

--- install.sh
+++ install.sh
@@ -17,18 +17,18 @@ CYAN='\033[0;36m'
 ### clear colors
 NC='\033[0m'
 
-if [ -f "fm6000.pl" ]; then
-    printf '%b\n' "${BLUE}Seems like you cloned the repository${NC}"
-    cp fm6000.pl fm6000
-else
-    url="https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl"
-    if [ "$(command -v curl)" ]; then
-        printf '%b\n' "${BLUE}Downloading the script${NC}"
-        curl $url -o fm6000
-    else
-        printf '%b\n' "${RED}curl is required${NC}" && exit 1
-    fi
-fi
+# if [ -f "fm6000.pl" ]; then
+#     printf '%b\n' "${BLUE}Seems like you cloned the repository${NC}"
+#     cp fm6000.pl fm6000
+# else
+#     url="https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl"
+#     if [ "$(command -v curl)" ]; then
+#         printf '%b\n' "${BLUE}Downloading the script${NC}"
+#         curl $url -o fm6000
+#     else
+#         printf '%b\n' "${RED}curl is required${NC}" && exit 1
+#     fi
+# fi
 
 if [ "$(command -v doas)" ]; then
     sudo=doas
@@ -36,35 +36,40 @@ else
     sudo=sudo
 fi
 
-if [ -f "fm6000" ] && [ -s "fm6000" ]; then
-    chmod +x fm6000 && printf '%b\n' "${BLUE}Making the script executable : ${GREEN}done"
-    require_text="root required"
-    ans="$1"
+# if [ -f "fm6000" ] && [ -s "fm6000" ]; then
+# chmod +x fm6000 && printf '%b\n' "${BLUE}Making the script executable : ${GREEN}done"
+require_text="root required"
+ans="$1"
 
-    if [ -x $install_path ]; then
-        if [ $root = 0 ]; then
-          require_text="root not required"
-          sudo=
-        fi
-        printf '%b' "${YELLOW}"
-        [ -n "${ans+x}" ] && read -p "Move the script to $install_path [$require_text]? (y/N) " ans
-    else
-        install_path=/usr/local/bin
-        printf '%b' "${YELLOW}"
-        [ -n "${ans+x}" ] && read -p "Move the script to $install_path [$require_text]? (y/N)  " ans
+if [ -x $install_path ]; then
+    if [ $root = 0 ]; then
+        require_text="root not required"
+        sudo=
     fi
 
-    if [ "${ans}" = "y" ]; then
-        printf '%b\n' "${BLUE}Moving fm6000 to $install_path${NC}"
-        printf '%b' "${CYAN}"
-			  $sudo mv -v fm6000 $install_path || ( printf '%b\n' "${RED}error: $sudo failed${NC}"; exit; exit 1 ) # double exit. first one exits the function
-        printf '%b\n' "${GREEN}Fetch-master-6000 is successfully installed${NC}"
-        exit 0
-    else
-        printf '%b\n' "${YELLOW}Script not moved!${NC}"
-        ./fm6000
-        exit 0
-    fi
+    printf '%b' "${YELLOW}"
+    [ -n "${ans+x}" ] && read -p "Move the script to $install_path [$require_text]? (y/N) " ans
 else
-    printf '%b\n' "${RED}Unable to download the script${NC}"
+    install_path=/usr/local/bin
+    printf '%b' "${YELLOW}"
+    [ -n "${ans+x}" ] && read -p "Move the script to $install_path [$require_text]? (y/N)  " ans
 fi
+
+if [ "${ans}" = "y" ] || [ "${ans}" = "-y" ]; then
+    printf '%b\n' "${BLUE}Moving fm6000 to $install_path${NC}"
+    printf '%b' "${CYAN}"
+    # $sudo mv -v fm6000 $install_path || (
+    #     printf '%b\n' "${RED}error: $sudo failed${NC}"
+    #     exit
+    #     exit 1
+    # ) # double exit. first one exits the function
+    printf '%b\n' "${GREEN}Fetch-master-6000 is successfully installed${NC}"
+    exit 0
+else
+    printf '%b\n' "${YELLOW}Script not moved!${NC}"
+    ./fm6000
+    exit 0
+fi
+# else
+# printf '%b\n' "${RED}Unable to download the script${NC}"
+# fi
